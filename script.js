3e327339446983a56d2d38419b6ce6e3126dada1
Yevhenii Malakhov <YevheniiMalakhov@Chainmalkos-Macbook-Air.local>
2021-07-10 15:01:26 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function saveData() {
+    a = prompt("Enter your data",)
+    let fruits = [0, "Banana", "Pear", "Apple", "Mango", "Kiwi"];
+    if ((a == 1 || a == 2 || a == 3 || a == 4 || a == 5)) {
+        console.log(fruits[a]);
+        return "Secret number!";
+        // } else if (a == 0){
+        //     return 0;
+    } else {
+        return a;
+    }
+};
+
+
